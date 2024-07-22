1ea26147604b273b0ba40dedc4a1da3eef51e8ec
tfihsdeR <erdincatay@yandex.com>
2024-07-22 16:45:36 +0300

    displayed user role in dashboard

--- components/pages/DashBoardPage.tsx
+++ components/pages/DashBoardPage.tsx
@@ -29,17 +29,18 @@ const DashBoardPage = () => {
 
     return (
         <div className='w-full h-full'>
-            <h1>Dashboard</h1>
+            <h1 className='mb-10'>Dashboard</h1>
 
-            <span className='mt-10 text-xl'>You can update or delete any post on home page! Because you are an Admin!</span>
+            <span className='text-xl'>You can update or delete any post on home page! Because you are an Admin!</span>
 
-            <div className='flex flex-col h-full mt-5'>
+            <div className='flex flex-col h-full mt-5 gap-2'>
                 <span className='text-2xl font-bold flex items-center mb-10'>Total Users:&nbsp;<span className='text-2xl font-bold'>{users?.length}</span></span>
 
                 {users?.map((user) => (
                     <div key={user.id} className='flex items-center lg:gap-10 max-lg:gap-5 bg-gray-400 max-w-max py-1 px-3 border-white border-2 rounded-md text-black'>
                         <span>{user.name}</span>
                         <span>{user.email}</span>
+                        <span>{user.role}</span>
                     </div>
                 ))}
             </div>
